0f4b5f82d25f799f1a9f5e4089733e1b0c7346f8
caaqil <abdukadir.sharif@just.edu.so>
2025-12-13 13:23:24 +0300

    connect backend to mangodb atlas

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,14 +2,13 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
+    await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
-
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
+    console.log('MongoDB Atlas connected');
   } catch (error) {
-    console.error(`Error: ${error.message}`);
+    console.error('MongoDB connection failed:', error.message);
     process.exit(1);
   }
 };
